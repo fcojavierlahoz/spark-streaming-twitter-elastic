8929734d709e0e62d1097bd80dc423edb8adcf42
JavierLahoz <fcojavierlahoz@users.noreply.github.com>
2021-08-10 18:47:29 +0200

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,1 +1,1 @@
-spark-submit --num-executors 2 --executor-memory 512MB target/scala-2.11/Twitter-ES-assembly-0.1-SNAPSHOT.jar uDXAMZZ3NkZuKTo6Ob7A MTrQuB4DCvQZDFUpTvnrHZk2blfhBYGbABEkECHs1wI 1382172042-Vfeku3FlB2CHxSjtznLgyYoUu5PpSfyyEMCv8iA 5g1TgFjGFFkUXD85MyCsjjjjT5z6awezYABELfL95xsr2
+spark-submit --num-executors 2 --executor-memory 512MB target/scala-2.11/Twitter-ES-assembly-0.1-SNAPSHOT.jar API_KEY API_SECRET TOKEN TOKEN_SECRET
